2d639ff16e09057a38d72185b60803f8079c061c
RyanRY27 <129965623+RyanRY27@users.noreply.github.com>
2023-10-21 12:31:18 +0800

    Add files via upload

---
+++ lab4/lab5.2/js/music-list.js
@@ -0,0 +1,34 @@
+let allMusic = [
+  {
+    name: "WELCOME2DTQ",
+    artist: "Hev Abi",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Malaking P",
+    artist: "E.J",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "WholeLotta$$$",
+    artist: "NOHESI & CA$HMAN",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "FLY$HIT",
+    artist: "LON Daniel & Costa Cashman",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Rapstar",
+    artist: "Flow-G",
+    img: "music-5",
+    src: "music-5"
+  },
+  
+  
+];
